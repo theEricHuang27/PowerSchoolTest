6d23401c6c0a4229efed71e4c139a70a46e15ec4
Chang-Chi Huang (student LM) <s013455@lm-20s013455.lmsd.org>
2019-01-11 14:12:05 -0500

    Initial Commit

---
+++ PowerSchoolTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PowerSchoolTest
+//
+//  Created by Chang-Chi Huang (student LM) on 1/11/19.
+//  Copyright © 2019 Chang-Chi Huang (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
