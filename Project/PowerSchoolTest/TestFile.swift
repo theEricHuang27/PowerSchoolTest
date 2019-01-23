5a656dffe347a36b0f0345dc983e9ec6210d645b
Aidan Barr Bono (student LM) <s012506@lm-20s012506.fios-router.home>
2019-01-23 10:40:12 -0500

    sdsadsa

---
+++ Project/PowerSchoolTest/TestFile.swift
@@ -0,0 +1,40 @@
+//
+//  TestFile.swift
+//  PowerSchoolTest
+//
+//  Created by Aidan Barr Bono (student LM) on 1/17/19.
+//  Copyright © 2019 Chang-Chi Huang (student LM). All rights reserved.
+//
+
+import Foundation
+import JavaScriptCore
+
+var context: JSContext? = {
+    let context = JSContext()
+    
+    // 1
+    guard let   // create path variable for the file
+        commonJSPath = Bundle.main.path(forResource: "test", ofType: "js", inDirectory: "./js") else {
+            print("Unable to read resource files.")
+            return nil
+    }
+    
+    // 2
+    do {
+        // create variable for the files contents, as a string
+        let common = try String(contentsOfFile: commonJSPath, encoding: String.Encoding.utf8)
+        // evaluate the script
+        _ = context?.evaluateScript(common)
+    } catch (let error) {
+        print("Error while processing script file: \(error)")
+    }
+    
+    return context
+}()
+
+func run() {
+
+    let testFunc = context?.objectForKeyedSubscript("x")
+    let result = testFunc?.call(withArguments: JSValue(5)) as Any
+    print(result)
+}
---
+++ Project/js/test.js
@@ -0,0 +1,9 @@
+"use-strict";
+
+function x(option) {
+    if (option === 5) {
+        return 5;
+    } else {
+        return 9;
+    }
+}
